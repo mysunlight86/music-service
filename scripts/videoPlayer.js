fc7a2556a7fc02956ff19733f1f2cba1fe0eb94d
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-07-28 14:38:13 +0300

    Added playback and pause when clicking on the player or the corresponding button

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,31 @@
 export const videoPlayerInit = () => {
-  console.log('v');
+  const videoPlayer = document.querySelector('.video-player');
+  const videoButtonPlay = document.querySelector('.video-button__play');
+  const videoButtonStop = document.querySelector('.video-button__stop');
+  const videorogress = document.querySelector('.video-progress');
+  const videoTimePassed = document.querySelector('.video-time__passed');
+  const videoTimeTotal = document.querySelector('.video-time__total');
+
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove('fa-pause');
+      videoButtonPlay.classList.add('fa-play');
+    } else {
+      videoButtonPlay.classList.add('fa-pause');
+      videoButtonPlay.classList.remove('fa-play');
+    }
+  }
+
+  const togglePlay = () => {
+    if (videoPlayer.paused){
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    }
+
+    toggleIcon();
+  };
+
+  videoPlayer.addEventListener('click', togglePlay);
+  videoButtonPlay.addEventListener('click', togglePlay);
 };
