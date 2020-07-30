67d04bd2e7755a0f2776e8113533d882147db4a2
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-07-30 17:38:46 +0300

    Added the audio player operation

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,5 @@
+import { addZero } from './supScript.js';
+
 export const videoPlayerInit = () => {
   const videoPlayer = document.querySelector('.video-player');
   const videoButtonPlay = document.querySelector('.video-button__play');
@@ -28,8 +30,6 @@ export const videoPlayerInit = () => {
     videoPlayer.pause();
     videoPlayer.currentTime = 0;
   }
-
-  const addZero = n => n < 10 ? '0' + n : n;
 
   videoPlayer.addEventListener('click', togglePlay);
   videoButtonPlay.addEventListener('click', togglePlay);
